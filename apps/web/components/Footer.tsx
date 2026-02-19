e9df23393ce42aa13eba36a27aeb813a04b60528
hyundeukkim-adlib88 <hyundeuk.kim@nethru.co.kr>
2026-02-19 14:56:06 +0900

    web footer 글자 변경 for 파이어베이스 빌드 테슽

--- apps/web/components/Footer.tsx
+++ apps/web/components/Footer.tsx
@@ -137,7 +137,7 @@ export default function Footer({ menus = [] }: FooterProps) {
               />
             </Link>
             <p className="footer-description">
-              Nethru의 데이터 수집(웹/모바일앱 로그)기술,<br />
+              <span style={{ color: '#ff9e4a', fontWeight: 'bold' }}>NETHRU</span>의 데이터 수집(웹/모바일앱 로그)기술,<br />
               고객중심관점의 분석 기술, 노하우가 집약된<br />
               통합 행동데이터 플랫폼
             </p>
